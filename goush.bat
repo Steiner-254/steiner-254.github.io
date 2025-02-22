08fd343f824cd58fb879fb50e8e22bfd82010639
steiner-254 <steineralvin3@gmail.com>
2025-02-23 02:04:31 +0300

    Portfolio Update

---
+++ goush.bat
@@ -0,0 +1,25 @@
+:: Steps To Create The Simple Automation Workflow Below - Works Only For Windows VS Code Terminal:
+:: 1. Create a file with extension `.bat` -> gpush.bat
+:: 2. Copy the code below into the file/Add your own commands if you wish
+:: 3. Open the Windows terminal on VS Code and run the command `.\gpush.bat` to execute the script - `gpush.bat` is our file name, feel free to customize
+:: 4. Successfully Automated
+:: -> Millionaire Vibes Inshallah! 🚀
+
+@echo off
+echo Adding all files to Git...
+git add .
+
+echo Checking Git status...
+git status
+
+echo Committing changes...
+git commit -m "Learning More About Web3 Security"
+
+echo Pushing to remote repository...
+git push
+
+echo "" Millionaire Vibes Inshallah! Happy Development ""
+echo "" Millionaire Vibes Inshallah! Happy Development ""
+echo "" Millionaire Vibes Inshallah! Happy Development ""
+echo "" Millionaire Vibes Inshallah! Happy Development ""
+echo "" Millionaire Vibes Inshallah! Happy Development ""
---
+++ gpush.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git add .
+git status
+git commit -m "Portfolio Update"
+git push
+
+## Inside WSL Windows Terminal Run This Command:
+## wsl ./gpush.sh
